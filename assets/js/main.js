0e0803ed32c3c1087c761447766f518c6b80f430
eugenyneo <eugeny.nenenko@gmail.com>
2020-03-05 11:28:14 +0200

    Adding changes...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,3 @@
-/*
-document.getElementById('hider').onclick = function() {
-    document.getElementById('flights_main').hidden = true;
-}
-*/
-
-/*
-"use strict";
-
-$(window).scroll(function() {
-
-    if ($(this).scrollTop() > 1000){
-
-        $('main_line').addClass("sticky");
-    }
-    else{
-        $('main_line').removeClass("sticky");
-    }
-});*/
-
-/*function search() {
-    var value = document.getElementById("search").value;
-    console.log("The value = " + value);
-}*/
-
-
 
 
 
